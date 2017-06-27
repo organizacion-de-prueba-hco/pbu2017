7eabddb5f1c97b6d4cf97217cad4d9cb236b5cd6
Wilian Fabian <few1986@gmail.com>
2017-06-27 15:15:49 -0500

    colegio comedor concesionariocomedor
    modelos
    Colegio
    Comedor
    ConcesionarioComedor

--- app/ConcesionarioComedor.php
+++ app/ConcesionarioComedor.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class ConcesionarioComedor extends Model
 {
-    //
+    protected $table='concesionario_comedors';
+    protected $fillable=[
+    'concesionario',
+    'estado'
+
+    ];
+
+    public function comedores(){
+    	return $this->hasMany('App\Comedors');
+    }
+    public function users(){
+    	return $this->belongsto('App\User');
+    }
+
+
 }
