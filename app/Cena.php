9df52fcd846bcfe8b4243b2b4e9749e123aa85dd
Wilian Fabian <few1986@gmail.com>
2017-06-27 13:19:40 -0500

    cena cambios
    cambios modelo cena

--- app/Cena.php
+++ app/Cena.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Cena extends Model
 {
-    //
+    
+    protected $table='cenas';
+    protected $fillable=[
+    'comedor_id',
+    'expediente_id'
+    ];
+
+
+	public function expediente(){
+  		return $this->belongsto('App\Expediente');
+
+  	}
+  	public function comedor(){
+  		return $this->belongsto('App\Comedor');
+
+  	}    
 }
