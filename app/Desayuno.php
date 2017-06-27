e170b80897ec331291699f42b54bf4d86268497b
Wilian Fabian <few1986@gmail.com>
2017-06-27 13:23:26 -0500

    modelo desayuno
    cambios modelo desayuno

--- app/Desayuno.php
+++ app/Desayuno.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Desayuno extends Model
 {
-    //
+   	protected $table='desayunos';
+   	protected $fillable=[
+   	'comedor_id',
+   	'expediente_id'
+   	];
+
+   	public function expediente(){
+   		return $this->belogsto('App\Expediente');
+   	}
+
+  	public function comedor(){
+  		return $this->belongsto('App\Comedor');
+
+  	}
+
+
 }
