4ffa991f823442d6fa2ddd4174d2a8a1aea1e234
Wilian Fabian <few1986@gmail.com>
2017-06-28 19:20:24 -0500

    creacion de plantilla
    creacion plantilla tester

---
+++ resources/views/users/jusu/expediente/tester.blade.php
@@ -0,0 +1,16 @@
+@extends('master.jusu')
+@section('activacion')
+	<?php
+		$oa='active';$a=''; $b='';$c=''; $c1='';$c2='';$c3='';$c4='';$d=''; $d1=''; $d2=''; $d3=''; $d4='';$e='';
+	?>
+@endsection
+@section('titulo','Tester')
+@section('estilos')
+@endsection
+@section('ruta')
+<ul class="breadcrumb">
+	<i class="ace-icon fa fa-list-alt"></i>	
+	<li class="active">Tester</li>
+</ul>
+@endsection
+@section('contenido')
